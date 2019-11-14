49c3e82954930b31b450ddd1fc23a2d2b1a195b5
Frankely <frankely.diaz@alliant.com>
2019-11-13 16:58:46 -0800

    removed unused tests

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RiotApi.PageControllerTest do
-  use RiotApi.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
