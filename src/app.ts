050963050001e3ecfe7f162be050e7210866fba1
roliver <roliverjavier@gmail.com>
2019-12-05 00:34:36 -0400

    adding tsoa for swagger documentation and route controllers using decorators #26

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import workflowsRouter from "./routers/workflowRouter";
+import { RegisterRoutes } from "./routers/routes";
+import * as swaggerUi from 'swagger-ui-express';
+
 const app = express();
 // TODO: Move midlewares setup to separate files under middlewares folder
 express.json();
 
-// Routers config
+RegisterRoutes(app);
+
+try {
+    const swaggerDocument = require('../dist/swagger.json');
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (err) {
+    console.log('Unable to load swagger.json', err);
+}
+
 app.use("/api/workflows", workflowsRouter);
 
 // Export express instance
