ebc246a51f5af7e8c3a1f878c45706df3eb8ea96
RoliverJavier <roliverjavier@gmail.com>
2019-12-05 10:58:40 -0400

    security vulnerabilities and empty line between decorator and function.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Route } from "tsoa";
 @Route("Users")
 export class UserController extends Controller {
     @Get()
-
     public async getUser() {
         return { name: "hello world" };
     }
